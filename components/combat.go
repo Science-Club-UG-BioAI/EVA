98997b9d5559604ab98b7f398ba046db38f97165
kkgrz13 <k.kozlowski684@gmail.com>
2025-04-02 00:06:04 +0200

    Feature: Basic combat Added Basic combat and some debug displays For now comat is on click and basic, to be updated

---
+++ components/combat.go
@@ -0,0 +1,25 @@
+package components
+
+type Combat interface {
+	Health() float64
+	AttackPower() float64
+	Damage(amount float64)
+}
+
+type BasicCombat struct {
+	health      float64
+	attackPower float64
+}
+
+func (b *BasicCombat) AttackPower() float64 {
+	return b.attackPower
+}
+func (b *BasicCombat) Health() float64 {
+	return b.health
+}
+
+func (b *BasicCombat) Damage(amount float64) {
+	b.health -= amount
+}
+
+var _ Combat = (*BasicCombat)(nil)
